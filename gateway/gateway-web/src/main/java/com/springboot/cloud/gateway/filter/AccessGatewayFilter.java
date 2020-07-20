1de0915b4cfc159c1bed7796b493a40de6aab858
limudadada <459735669@qq.com>
2020-07-20 17:10:47 +0800

    feature:接入日志mdc追踪

--- gateway/gateway-web/src/main/java/com/springboot/cloud/gateway/filter/AccessGatewayFilter.java
+++ gateway/gateway-web/src/main/java/com/springboot/cloud/gateway/filter/AccessGatewayFilter.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.cloud.auth.client.service.IAuthService;
 import com.springboot.cloud.gateway.service.IPermissionService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
@@ -17,6 +19,8 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.function.Consumer;
 
 /**
  * 请求url权限校验
@@ -48,24 +52,49 @@ public class AccessGatewayFilter implements GlobalFilter {
      */
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        ServerHttpRequest request = exchange.getRequest();
-        String authentication = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-        String method = request.getMethodValue();
-        String url = request.getPath().value();
-        log.debug("url:{},method:{},headers:{}", url, method, request.getHeaders());
-        //不需要网关签权的url
-        if (authService.ignoreAuthentication(url)) {
-            return chain.filter(exchange);
-        }
+        try {
+            ServerHttpRequest request = exchange.getRequest();
+            String reqContextId = request.getHeaders().getFirst(GlobalTraceIdContext.REQUESTID_HEADER_KEY);
+            if (StringUtils.isNotBlank(reqContextId)) {
+                MDC.put(GlobalTraceIdContext.REQUESTID_HEADER_KEY, reqContextId);
+            } else {
+                reqContextId = GlobalTraceIdContext.getUUID();
+                MDC.put(GlobalTraceIdContext.REQUESTID_HEADER_KEY, reqContextId);
+            }
+            GlobalTraceIdContext.setRequestId(reqContextId);
+            String finalReqContextId = reqContextId;
 
-        //调用签权服务看用户是否有权限，若有权限进入下一个filter
-        if (permissionService.permission(authentication, url, method)) {
-            ServerHttpRequest.Builder builder = request.mutate();
-            //TODO 转发的请求都加上服务间认证token
-            builder.header(X_CLIENT_TOKEN, "TODO zhoutaoo添加服务间简单认证");
-            //将jwt token中的用户信息传给服务
-            builder.header(X_CLIENT_TOKEN_USER, getUserToken(authentication));
-            return chain.filter(exchange.mutate().request(builder.build()).build());
+            String authentication = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+            String method = request.getMethodValue();
+            String url = request.getPath().value();
+            log.debug("finalReqContextId is {}:url:{},method:{},headers:{}", finalReqContextId, url, method, request.getHeaders());
+            //不需要网关签权的url
+            if (authService.ignoreAuthentication(url)) {
+                Consumer<HttpHeaders> httpHeaders = httpHeader -> {
+                    httpHeader.set(GlobalTraceIdContext.REQUESTID_HEADER_KEY, finalReqContextId);
+                };
+                ServerHttpRequest serverHttpRequest = exchange.getRequest().mutate().headers(httpHeaders).build();
+                exchange.mutate().request(serverHttpRequest).build();
+                return chain.filter(exchange);
+            }
+
+            //调用签权服务看用户是否有权限，若有权限进入下一个filter
+            if (permissionService.permission(authentication, url, method)) {
+                Consumer<HttpHeaders> httpHeaders = httpHeader -> {
+                    httpHeader.set(GlobalTraceIdContext.REQUESTID_HEADER_KEY, finalReqContextId);
+                    httpHeader.set(X_CLIENT_TOKEN, "TODO zhoutaoo添加服务间简单认证");
+                    httpHeader.set(X_CLIENT_TOKEN_USER, getUserToken(authentication));
+                };
+                ServerHttpRequest serverHttpRequest = exchange.getRequest().mutate().headers(httpHeaders).build();
+                exchange.mutate().request(serverHttpRequest).build();
+                return chain.filter(exchange);
+            }
+        } catch (Exception e) {
+            log.error("AccessGatewayFilter error", e);
+        } finally {
+            MDC.clear();
+            //避免tomcat回收线程造成过期数据
+            GlobalTraceIdContext.setRequestId(null);
         }
         return unauthorized(exchange);
     }
