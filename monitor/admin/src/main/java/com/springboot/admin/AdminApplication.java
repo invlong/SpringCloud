d0814ce8187124a580fb088de91b4e60b71202c3
limudadada <459735669@qq.com>
2020-08-15 17:13:13 +0800

    增加钉钉报警

--- monitor/admin/src/main/java/com/springboot/admin/AdminApplication.java
+++ monitor/admin/src/main/java/com/springboot/admin/AdminApplication.java
@@ -1,9 +1,11 @@
 package com.springboot.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +13,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
+    }
+
+    @Bean
+    public MonitorNotify dingDingNotifier(InstanceRepository repository) {
+        return new MonitorNotify(repository);
     }
 }
