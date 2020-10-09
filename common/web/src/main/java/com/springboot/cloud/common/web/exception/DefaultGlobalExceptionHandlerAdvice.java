b2b3ea64500b499b25a45cc09901073cb725e029
invlong <pro.invlong@gmail.com>
2020-10-09 15:51:10 +0800

    fix: 修复全局异常可能打印不出来的问题

--- common/web/src/main/java/com/springboot/cloud/common/web/exception/DefaultGlobalExceptionHandlerAdvice.java
+++ common/web/src/main/java/com/springboot/cloud/common/web/exception/DefaultGlobalExceptionHandlerAdvice.java
@@ -4,6 +4,7 @@ import com.springboot.cloud.common.core.entity.vo.Result;
 import com.springboot.cloud.common.core.exception.BaseException;
 import com.springboot.cloud.common.core.exception.SystemErrorType;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -17,37 +18,38 @@ public class DefaultGlobalExceptionHandlerAdvice {
 
     @ExceptionHandler(value = {MissingServletRequestParameterException.class})
     public Result missingServletRequestParameterException(MissingServletRequestParameterException ex) {
-        log.error("missing servlet request parameter exception:{}", ex.getMessage());
+        log.error("missing servlet request parameter exception:{}", ExceptionUtils.getStackTrace(ex));
         return Result.fail(SystemErrorType.ARGUMENT_NOT_VALID);
     }
 
     @ExceptionHandler(value = {MultipartException.class})
     public Result uploadFileLimitException(MultipartException ex) {
-        log.error("upload file size limit:{}", ex.getMessage());
+        log.error("upload file size limit:{}", ExceptionUtils.getStackTrace(ex));
         return Result.fail(SystemErrorType.UPLOAD_FILE_SIZE_LIMIT);
     }
 
     @ExceptionHandler(value = {MethodArgumentNotValidException.class})
     public Result serviceException(MethodArgumentNotValidException ex) {
-        log.error("service exception:{}", ex.getMessage());
+        log.error("service exception:{}", ExceptionUtils.getStackTrace(ex));
         return Result.fail(SystemErrorType.ARGUMENT_NOT_VALID, ex.getBindingResult().getFieldError().getDefaultMessage());
     }
 
     @ExceptionHandler(value = {DuplicateKeyException.class})
     public Result duplicateKeyException(DuplicateKeyException ex) {
-        log.error("primary key duplication exception:{}", ex.getMessage());
+        log.error("primary key duplication exception:{}", ExceptionUtils.getStackTrace(ex));
         return Result.fail(SystemErrorType.DUPLICATE_PRIMARY_KEY);
     }
 
     @ExceptionHandler(value = {BaseException.class})
     public Result baseException(BaseException ex) {
-        log.error("base exception:{}", ex.getMessage());
+        log.error("base exception:{}", ExceptionUtils.getStackTrace(ex));
         return Result.fail(ex.getErrorType());
     }
 
     @ExceptionHandler(value = {Exception.class})
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Result exception() {
+    public Result exception(Exception ex) {
+        log.error("exception:{}", ExceptionUtils.getStackTrace(ex));
         return Result.fail();
     }
 
